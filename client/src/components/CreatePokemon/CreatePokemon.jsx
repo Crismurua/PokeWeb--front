ad33db97a5d33f9cbe392a2977e3c0fd40358ab4
Cristian Murua <cristianmurua1995@gmail.com>
2022-09-29 01:24:34 -0300

    react-form

---
+++ client/src/components/CreatePokemon/CreatePokemon.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+const CreatePokemon = () => {
+    const initialState = {
+        name: "",
+        img: "",
+        hp: 0,
+        attack: 0,
+        defense: 0,
+        speed: 0,
+        types: [{id:0}],
+        height: 0,
+        weight: 0,
+    };
+    let dispatch = useDispatch();
+    let [input, setInput] = React.useState(initialState);
+
+    React.useEffect(() => {
+        setInput(prev => ({ ...prev, [input.name]:input.value}))
+    }, [])
+
+    let handleOnSubmit = (e) => {
+        e.preventDefault();
+        dispatch(CreatePokemon(input));
+        setInput(initialState);
+    };
+
+    return (
+        <div>
+            <form onSubmit={(e) => handleOnSubmit(e)}>
+                <label>Name: </label>
+                <input type="text"
+                        name="name"
+                        value={input.name}
+                        onChange={input}
+                        />
+                <label>IMG: </label>
+                <input type="text"
+                        name="img"
+                        value={input.img}
+                        onChange={input}
+                        />
+                <label>HP: </label>
+                <input type="number"
+                        min="10"
+                        max="100"
+                        name="hp"
+                        value={input.hp}
+                        onChange={input}
+                        />
+                <label>ATTACK: </label>
+                <input type="number"
+                        min="10"
+                        max="100"
+                        name="attack"
+                        value={input.attack}
+                        onChange={input}
+                        />
+                <label>DEFENSE: </label>
+                <input type="number"
+                        min="10"
+                        max="100"
+                        name="defense"
+                        value={input.defense}
+                        onChange={input}
+                        />
+                <label>SPEED: </label>
+                <input type="number"
+                        min="10"
+                        max="100"
+                        name="speed"
+                        value={input.speed}
+                        onChange={input}
+                        />
+                <label>HEIGHT: </label>
+                <input type="number"
+                        min="10"
+                        max="100"
+                        name="height"
+                        value={input.height}
+                        onChange={input}
+                        />
+                <label>WEIGHT: </label>
+                <input type="number"
+                        min="10"
+                        max="100"
+                        name="weight"
+                        value={input.weight}
+                        onChange={input}
+                        />
+                <label>TYPES: </label>
+                <input type=""
+                        name="types"
+                        value={input.types}
+                        onChange={input}
+                        />
+
+                <button type="submit">Create Pokemon</button>        
+            </form>
+        </div>
+    );
+
+};
+
+export default CreatePokemon;
--- client/src/components/PokemonDetail/PokemonDetail.jsx
+++ client/src/components/PokemonDetail/PokemonDetail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import * as actions from "../../redux/actions/index.js";
-import {Types} from "../Types/Types.jsx";
+import Types from "../Types/Types.jsx";
 
 const PokemonDetail = (props) => {
     let params = useParams(props) 
@@ -26,11 +26,11 @@ const PokemonDetail = (props) => {
             <h4>Stats</h4>
             <ul>
                 <li>HP: {detail.hp}</li>
-                <li>DMG: {detail.attack}</li>
+                <li>ATK: {detail.attack}</li>
                 <li>DEF: {detail.defense}</li>
                 <li>SPD: {detail.speed}</li>
             </ul>
-            <p>Height: {detail.height} KG</p>
+            <p>Height: {detail.height} CM</p>
             <p>Weight: {detail.height} KG</p>
             <h4>Types</h4>
             <Types />
