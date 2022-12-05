12c4ec7dd208ae03e7dcfb92047b9ecfa38bb070
Cristian Murua <cristianmurua1995@gmail.com>
2022-12-05 01:52:04 -0300

    msg

--- client/src/index.js
+++ client/src/index.js
@@ -10,13 +10,13 @@ import store from './redux/store';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
