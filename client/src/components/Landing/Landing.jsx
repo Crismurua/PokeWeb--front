fd6c2a8e8db52da22736c256cbb434e6b4262d8a
Cristian Murua <cristianmurua1995@gmail.com>
2022-12-05 01:46:25 -0300

    fix

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./Landing.css"
 
 
 const Landing = () => {
-
+    const navigate = useHistory();
     return (
         <div className="landing">
-            <Link to="/pokemons" className="enter">WELCOME</Link>
+            <button  className="enter" onClick={()=> navigate.push('/pokemons')}>WELCOME</button>
             <img src="media/pokemon-logo.png" className="land-logo" alt="logo" />
             <img src="media/landing.png" className="wall" alt="pokeball" />
 
